d9f7bf70225f496f2e4ee1da692d717d43a45f75
rajkarkhanis <rkarkhanis00@gmail.com>
2023-11-22 14:57:18 +0530

    drag and drop task lists

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,26 +1,86 @@
-import { initialLists, initialTasks } from "@/lib/types";
-import { DndContext } from "@dnd-kit/core";
-import { SortableContext } from "@dnd-kit/sortable";
+import { List, initialLists, initialTasks } from "@/lib/types";
+import {
+    DndContext,
+    DragEndEvent,
+    DragOverEvent,
+    DragOverlay,
+    DragStartEvent,
+} from "@dnd-kit/core";
+import { SortableContext, arrayMove } from "@dnd-kit/sortable";
 import { useMemo, useState } from "react";
 import TaskList from "./TaskList";
 
 const KanbanBoard = () => {
     const [tasks] = useState(initialTasks);
-    const [lists] = useState(initialLists);
-    
+    const [lists, setLists] = useState(initialLists);
+
+    const [activeList, setActiveList] = useState<List | null>(null);
+
     const listIds = useMemo(() => lists.map((c) => c.id), [lists]);
 
+    const onDragStart = (event: DragStartEvent) => {
+        console.log(event);
+        const currentElement = event.active.data.current;
+
+        if (currentElement?.type === "List") {
+            setActiveList(currentElement?.list);
+            return;
+        }
+    };
+
+    const onDragOver = (event: DragOverEvent) => {
+        console.log(event);
+    };
+
+    const onDragEnd = (event: DragEndEvent) => {
+        setActiveList(null);
+        const { active, over } = event;
+
+        // if List was not dragged over anything
+        if (!over) return;
+
+        // if List was dragged, but over itself
+        if (active.id === over.id) return;
+
+        // are we actually dragging a list?
+        const isActiveAList = active.data.current?.type === "List";
+        if (!isActiveAList) return;
+
+        const activeIndex = lists.findIndex((l) => l.id === active.id);
+        const overIndex = lists.findIndex((l) => l.id === over?.id);
+
+        const newLists = arrayMove(lists, activeIndex, overIndex);
+        setLists(newLists);
+    };
+
     return (
-        <DndContext>
-            <SortableContext items={listIds}>
-                {lists.map((list) => (
+        <DndContext
+            onDragStart={onDragStart}
+            onDragOver={onDragOver}
+            onDragEnd={onDragEnd}
+        >
+            <div className="flex gap-4 items-center flex-row justify-center">
+                <SortableContext items={listIds}>
+                    {lists.map((list) => (
+                        <TaskList
+                            key={list.id}
+                            list={list}
+                            tasks={tasks.filter((t) => t.columnId === list.id)}
+                        />
+                    ))}
+                </SortableContext>
+            </div>
+            <DragOverlay>
+                {activeList && (
                     <TaskList
-                        key={list.id}
-                        list={list}
-                        tasks={tasks.filter((t) => t.columnId === list.id)}
+                        isOverlay
+                        list={activeList}
+                        tasks={tasks.filter(
+                            (t) => t.columnId === activeList.id
+                        )}
                     />
-                ))}
-            </SortableContext>
+                )}
+            </DragOverlay>
         </DndContext>
     );
 };
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,19 +1,61 @@
 import { List, Task } from "@/lib/types";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { useMemo } from "react";
-import { SortableContext } from "@dnd-kit/sortable";
+import { SortableContext, useSortable } from "@dnd-kit/sortable";
 import TaskCard from "./TaskCard";
+import { CSS } from "@dnd-kit/utilities";
+import { cva } from "class-variance-authority";
 
 interface TaskListProps {
     list: List;
     tasks: Task[];
+    isOverlay?: boolean;
 }
 
-const TaskList = ({ list, tasks }: TaskListProps) => {
+const TaskList = ({ list, tasks, isOverlay }: TaskListProps) => {
     const taskIds = useMemo(() => tasks.map((t) => t.id), [tasks]);
 
+    const {
+        setNodeRef,
+        attributes,
+        listeners,
+        transform,
+        transition,
+        isDragging,
+    } = useSortable({ id: list.id, data: { type: "List", list } });
+
+    const style = {
+        transition,
+        transform: CSS.Transform.toString(transform),
+    };
+
+    const variants = cva(
+        "h-[500px] max-h-[500px] w-[350px] max-w-full bg-secondary flex flex-col shrink-0 snap-center",
+        {
+            variants: {
+                dragging: {
+                    default: "border-2 border-transparent",
+                    over: "ring-2 ring-orange-200 opacity-30",
+                    overlay: "ring-2 ring-orange-400",
+                },
+            },
+        }
+    );
+
     return (
-        <Card>
+        <Card
+            ref={setNodeRef}
+            {...attributes}
+            {...listeners}
+            style={style}
+            className={variants({
+                dragging: isOverlay
+                    ? "overlay"
+                    : isDragging
+                    ? "over"
+                    : undefined,
+            })}
+        >
             <CardHeader>
                 <CardTitle>{list.title}</CardTitle>
             </CardHeader>
