6020d6d41a1388baa99e7741ac3c1cda7286b7b4
Raj Karkhanis <rkarkhanis00@gmail.com>
2024-11-29 16:20:14 +0530

    feat: add vercel analytics and insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Analytics } from "@vercel/analytics/react";
 import KanbanBoard from "./components/KanbanBoard";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
-  return <KanbanBoard />;
+  return (
+    <div>
+      <KanbanBoard />
+      <Analytics />
+      <SpeedInsights />
+    </div>
+  );
 }
 
 export default App;
